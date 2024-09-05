b7a09121fc2b0dd4fbbc992769f68615ff0a7ae6
QuiqueVladimir <U202022365@upc.edu.pe>
2024-09-05 10:54:56 -0500

    feat(header): add header and background

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,45 @@
+function AnimatedToggle() {
+    let toggle = document.querySelector(".toggle");
+    toggle.classList.toggle("active");
+}
+
+$.getJSON('public/assets/scripts/Idioma.json', function(json) {
+    $(function() {
+        let defaultLang = 'es';
+        $('[data-current-lang]').attr('data-current-lang', defaultLang);
+
+        $('.lang').each(function(index, element) {
+            let key = $(this).attr('key');
+            let placeholderExist = $(this).attr('placeholder');
+
+            if (placeholderExist) {
+                $(this).attr('placeholder', json[defaultLang][key]);
+            } else {
+                $(this).text(json[defaultLang][key]);
+            }
+        });
+
+        $('.translate').click(function(event) {
+            event.preventDefault();
+
+            let lang = $(this).attr('data-lang');
+            let currentLang = $('[data-current-lang]').attr('data-current-lang');
+
+            if (lang === currentLang) {
+                return;
+            }
+
+            $('.lang').each(function(index, element) {
+                let key = $(this).attr('key');
+                let placeholderExist = $(this).attr('placeholder');
+
+                if (placeholderExist) {
+                    $(this).attr('placeholder', json[lang][key]);
+                } else {
+                    $(this).text(json[lang][key]);
+                }
+            });
+            $('[data-current-lang]').attr('data-current-lang', lang);
+        });
+    });
+});
